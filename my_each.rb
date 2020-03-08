c525ce36932daadb9ec161cbed04c9eb92ede5a2
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-08 18:39:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each (words)# put argument(s) here
   # code here
+  if block_given?
+
+  i = 0
+  while i < words.length
+    yield words[i]
+    i = i +1
+  end
+  words
+end
+
+words.each do |word|
+  puts word
 end
